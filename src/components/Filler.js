bc0a52677d1a139fbc73f74c47a9324b4bb2ddc8
Britney Smith <brit.smith7@gmail.com>
2019-10-18 12:10:44 -0400

    Start progress bar + filler components

---
+++ src/components/Filler.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Filler = () => {
+    return(
+        <FillerDiv />
+    );
+}
+
+const FillerDiv = styled.div`
+
+`;
+
+export default Filler;
---
+++ src/components/ProgressBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from "styled-components";
+
+const ProgressBar = () => {
+    return(
+        <ProgressBarDiv />
+    );
+}
+
+const ProgressBarDiv = styled.div`
+
+`;
+
+export default ProgressBar;
