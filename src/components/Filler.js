32e691adb7a6b0ca4a13bab71146035a360a38e7
Britney Smith <brit.smith7@gmail.com>
2019-10-18 13:36:04 -0400

    Add filler style

--- src/components/Filler.js
+++ src/components/Filler.js
@@ -8,7 +8,10 @@ const Filler = () => {
 }
 
 const FillerDiv = styled.div`
-
+    background: #09d3ac;
+    height: 100%;
+    border-radius: inherit;
+    transition: width .2s ease-in;
 `;
 
 export default Filler;
